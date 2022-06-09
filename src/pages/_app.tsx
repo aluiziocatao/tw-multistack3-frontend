2104befdce2da142e628a423cc4f7ca2882e7a6f
Aluízio Catão <aluizio2210@gmail.com>
2022-06-08 22:42:10 -0500

    Classroom 05

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
 import tema from '../ui/themes/tema'
 import Cabecalho from '../ui/Components/Cabecalho/Cabecalho'
+import CabecalhoAdmin from '../ui/Components/CabecalhoAdmin/CabecalhoAdmin'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();  
   return (
     <ThemeProvider theme={tema}>
-      <Cabecalho />
+      { router.pathname === '/' ? <Cabecalho /> : <CabecalhoAdmin />}
       <Component {...pageProps} />
     </ThemeProvider>
   )
